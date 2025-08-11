19a30d80220d103a33c7e3eac81782cbc3678d95
Vira <guidevirgate@gmail.com>
2025-08-11 20:13:21 +0500

    bs

--- main_app/signals.py
+++ main_app/signals.py
@@ -2,25 +2,31 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.core.mail import send_mail
 from django.conf import settings
+from main_app.models.model_news.news import NewsSubscriber,News
 
-from .models import News, NewsSubscriber  # путь к моделям должен быть корректным
-
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from django.core.mail import send_mail
-from django.conf import settings
-from .models import News, NewsSubscriber
 
 @receiver(post_save, sender=News)
-def notify_subscribers(sender, instance, created, **kwargs):
-    if created:
-        subscribers = NewsSubscriber.objects.select_related('user').all()
-        for subscriber in subscribers:
+def send_news_to_subscribers(sender, instance, created, **kwargs):
+    if created:  # новость только что создана
+        subject = f"Новая новость: {instance.title}"
+        message = f"{instance.main_text[:300]}...\n\nПодробнее: {settings.SITE_URL}/news/{instance.id}/"
+
+        # собираем список email
+        emails = []
+        for sub in NewsSubscriber.objects.all():
+            if sub.user and sub.user.email:
+                emails.append(sub.user.email)
+            elif sub.email:
+                emails.append(sub.email)
+
+        # убираем дубликаты
+        emails = list(set(emails))
+
+        if emails:
             send_mail(
-                subject=f"📰 Новая новость: {instance.title}",
-                message=instance.main_text[:500] + '...',
-                from_email=settings.DEFAULT_FROM_EMAIL,
-                recipient_list=[subscriber.user.email],
-                fail_silently=True
+                subject,
+                message,
+                settings.DEFAULT_FROM_EMAIL,
+                emails,
+                fail_silently=False,
             )
-            
